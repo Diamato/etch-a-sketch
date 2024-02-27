f8f1aa91ce0ce12255574e10a5ca3d200eb38d68
Igor Grafov <grafov2711@gmail.com>
2024-02-27 22:53:00 +0300

    added script

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const CONTAINER = document.querySelector("#container");
+document.addEventListener("DOMContentLoaded", createBoard(16));
 
-for (let i = 1; i <= 256; i++) {
-  let div = document.createElement("div");
-  div.className = "box";
-  CONTAINER.appendChild(div);
+// -------------
+
+function createBoard(size) {
+  let board = document.querySelector(".container");
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let numDivs = size * size;
+
+  for (let i = 0; i < numDivs; i++) {
+    let div = document.createElement("div");
+    board.insertAdjacentElement("beforeend", div);
+  }
 }
